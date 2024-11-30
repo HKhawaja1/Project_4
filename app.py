63374bb360c2535370a7ae763f3ef17bedee9c05
Hamzah Khawaja <hamzahkhawaja1@gmail.com>
2024-11-30 15:17:59 +0000

    More images added on homepage and catalog page.

--- app.py
+++ app.py
@@ -24,6 +24,18 @@ def sale():
 def contact():
     return render_template("contact.html")
 
+@app.route('/candy')
+def candy():
+    return render_template('candy.html')
+
+@app.route('/chocolate')
+def chocolate():
+    return render_template('chocolate.html')
+
+@app.route('/cookies')
+def cookies():
+    return render_template('cookies.html')
+
 # Run the application in debug mode (for development purposes)
 if __name__ == "__main__":
     app.run(debug=True)
